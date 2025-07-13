e83411574c10e5f7f6e628a8504894c5a05f47ed
sebasbit <sebastian.torres0426@gmail.com>
2025-07-13 11:14:41 -0500

    feat: remove MongoDB service

--- mongo-init.js
+++
@@ -1,7 +0,0 @@
-db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
-
-db.createUser({
-  user: process.env.MONGO_APP_USER,
-  pwd: process.env.MONGO_APP_PASSWORD,
-  roles: [{ role: "readWrite", db: process.env.MONGO_INITDB_DATABASE }],
-});
