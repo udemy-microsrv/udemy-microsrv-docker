0b45d16bb98eda04fd258716a3ea19afebcb6ff3
sebasbit <sebastian.torres0426@gmail.com>
2025-07-12 19:23:00 -0500

    feat: add MongoDB and udemy-microsrv-auth services

---
+++ mongo-init.js
@@ -0,0 +1,7 @@
+db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
+
+db.createUser({
+  user: process.env.MONGO_APP_USER,
+  pwd: process.env.MONGO_APP_PASSWORD,
+  roles: [{ role: "readWrite", db: process.env.MONGO_INITDB_DATABASE }],
+});
